4a51f930ae46dbca671ca6b50c30dcc668fc3ca6
ryosuke080 <yurilukebody@yahoo.co.jp>
2019-01-22 11:28:34 +0900

    no message

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function changeDisplay(){
+  var str = document.getElementById("target","button");
+  if(str.style.display = "none"){
+    str.style.display = "block";
+  }else{
+    str.style.display = "none";
+  }
+}
